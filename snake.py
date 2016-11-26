3afc836f448cb2a96179050f2ee7be0c9bcba392
martin <xxm0703@abv.bg>
2016-11-26 18:06:40 +0200

    no_magic_numbers_and_working_walls

--- snake.py
+++ snake.py
@@ -5,12 +5,16 @@ pygame.init()
 
 wide = 800
 high = 600
-snake_x = wide / 2  # To be at the center of the screen
+snake_step = 10
+sect_size = 10 # Size of one box
+snake_x = wide / 2  # Center of the screen
 snake_y = high / 2
-apple_x = random.randint(10, wide - 10)  # Some space before border
+apple_x = random.randint(10, wide - 10)  # Some space before the end of the screen
 apple_y = random.randint(20, high - 10)
+apple_size = 10
 wall_x = 0
 wall_y = 0
+wall_len = 40
 
 RED = [255, 0, 0]
 GREEN = [0, 255, 0]
@@ -22,7 +26,12 @@ D_BLUE = [52, 63, 197]
 D_GREEN = [0, 128, 0]
 T_BLACK = [0, 0, 0, 50]
 
-slither = pygame.image.load('photo.png')
+home_screen = pygame.image.load('photo.png')
+
+def game_over():
+    message("Game Over!", RED, 35)
+    pygame.display.update()
+    pygame.time.delay(2000)
 
 
 def message(msg, color=BLACK, size=25, line=1, msg_x=wide/2, msg_y=high/3):
@@ -38,7 +47,7 @@ def eyes(direction, x, y):
         return [[x + 2, y + 6], [x + 2, y + 2]]
     elif direction is "DOWN":
         return [[x + 6, y + 6], [x + 2, y + 6]]
-    elif direction is "RIGHT":
+    else:
         return [[x + 6, y + 2], [x + 6, y + 6]]
 
 screen = pygame.display.set_mode((wide, high))
@@ -49,7 +58,7 @@ PAUSE = False
 PLAY = True
 in_game = False
 change_x = 0
-change_y = -10
+change_y = -snake_step
 state = "UP"
 screen.fill(WHITE)
 while PLAY:
@@ -57,7 +66,7 @@ while PLAY:
     wall_list = []
     pos_list.insert(0, (snake_x, snake_y))
     apple_count = 0
-    screen.blit(slither, [100, 0])
+    screen.blit(home_screen , [100, 0])
     message("This is my 'Snake' game. I hope you like it.", BLACK, 30)
     message("Press any key to continue...", BLACK, 30, 2)
     pygame.display.update()
@@ -81,19 +90,19 @@ while PLAY:
                     pygame.display.update()
                     PAUSE = True
                 elif event.key == pygame.K_LEFT and state != "RIGHT":
-                    change_x = -10
+                    change_x = -snake_step
                     change_y = 0
                     state = "LEFT"
                 elif event.key == pygame.K_RIGHT and state != "LEFT":
-                    change_x = 10
+                    change_x = snake_step
                     change_y = 0
                     state = "RIGHT"
                 elif event.key == pygame.K_UP and state != "DOWN":
-                    change_y = -10
+                    change_y = -snake_step
                     change_x = 0
                     state = "UP"
                 elif event.key == pygame.K_DOWN and state != "UP":
-                    change_y = 10
+                    change_y = snake_step
                     change_x = 0
                     state = "DOWN"
         while PAUSE:
@@ -111,13 +120,19 @@ while PLAY:
         pos_list.insert(0, (snake_x, snake_y))
         pos_list.pop(-1)
 
-        if snake_x < 0 or snake_y < 0 or snake_x > wide or snake_y > high or (-10 < wall_x - snake_x < 10 and -10 < wall_y - snake_y < 10) or pos_list.count(pos_list[0]) == 2:
-            message("Game Over!", RED, 35)
-            pygame.display.update()
-            pygame.time.delay(2000)
-            in_game = False
-
-        elif -10 < apple_x - snake_x < 10 and -10 < apple_y - snake_y < 10:
+        for (wall_x, wall_y) in wall_list:
+            if wall_x % 2 == 0:
+                if -wall_len < wall_x-snake_x < sect_size and -sect_size < wall_y-snake_y < sect_size:
+                    game_over()
+                    in_game = False
+            else:
+                if -sect_size < wall_x-snake_x < sect_size and -wall_len < wall_y-snake_y < sect_size:
+                    game_over()
+                    in_game = False
+        if snake_x < 0 or snake_y < 0 or snake_x > wide or snake_y > high or pos_list.count(pos_list[0]) == 2:
+                game_over()
+                in_game = False
+        elif -apple_size < apple_x-snake_x < apple_size and -apple_size < apple_y-snake_y < apple_size:
             pygame.mixer.music.play()
             apple_x = random.randint(10, wide - 10)
             apple_y = random.randint(20, high - 10)
@@ -135,20 +150,20 @@ while PLAY:
         screen.fill(D_BLUE)
 
         message("P for pause", T_BLACK, 15, 1, 70, -20)
-        pygame.draw.rect(screen, RED, [apple_x, apple_y, 9, 9])
+        pygame.draw.rect(screen, RED, [apple_x, apple_y, apple_size, apple_size])
         for (wall_x, wall_y) in wall_list:
             if wall_x % 2 == 0:
-                pygame.draw.line(screen, YELLOW, (wall_x, wall_y), (wall_x + 40, wall_y), 10)
+                pygame.draw.line(screen, YELLOW, (wall_x, wall_y), (wall_x + wall_len, wall_y), 10)
             else:
-                pygame.draw.line(screen, YELLOW, (wall_x, wall_y), (wall_x, wall_y + 40), 10)
+                pygame.draw.line(screen, YELLOW, (wall_x, wall_y), (wall_x, wall_y + wall_len), 10)
         for cord in pos_list:
             if cord is not pos_list[0]:
-                pygame.draw.rect(screen, D_GREEN, [cord[0], cord[1], 10, 10])
+                pygame.draw.rect(screen, D_GREEN, [cord[0], cord[1], sect_size, sect_size])
             else:
-                pygame.draw.rect(screen, GREEN, [cord[0], cord[1], 10, 10])
+                pygame.draw.rect(screen, GREEN, [cord[0], cord[1], sect_size, sect_size])
                 for eye_cord in eyes(state, cord[0], cord[1]):
                     pygame.draw.rect(screen, BLACK, [eye_cord[0], eye_cord[1], 2, 2])
-        pygame.draw.rect(screen, BLACK, [0, 0, 800, 600], 2)
+        pygame.draw.rect(screen, BLACK, [0, 0, wide, high], 2)
         pygame.display.update()
         clock.tick(10)
     screen.fill(WHITE)
